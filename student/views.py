0e72558bfb8c1dac7a642da64ffa6e826cfad615
Nithin0029 <nithelanjayakumar@gmail.com>
2026-07-09 09:44:36 +0530

    Adding HTML code

--- student/views.py
+++ student/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect, render,get_object_or_404
+
+from student.forms import StudentForm
 from .models import student
 
 def home(request):
@@ -21,3 +23,35 @@ def student_detail(request, student_id):
     }
 
     return render(request, 'students/student_detail.html', context)
+
+def student_create(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(request, 'students/student_form.html', {'form': form, 'title': 'Create Student'})
+
+
+
+
+def student_update(request, student_id):
+    student_obj = get_object_or_404(student, id=student_id)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES, instance=student_obj)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student_obj)
+
+    return render(request, 'students/student_form.html', {'form': form, 'title': 'Update Student'})
+
+def student_delete(request, student_id):
+    student_obj = get_object_or_404(student, id=student_id)
+    if request.method == 'POST':
+        student_obj.delete()
+        return redirect('student_list')
+    return render(request, 'students/student_delete.html', {'student': student_obj})
